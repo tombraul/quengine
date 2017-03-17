1dc29777f5a40fc6dd8793196a70648b8efcd1c9
Tom Braul <tobr772@gmail.com>
2017-03-17 14:59:31 +0100

    replace bootstrap with bulma.css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
+// uses quotes-on-design api to get random quotes
 function getQuote () {
 	var title = document.getElementById('title');
 	var content = document.getElementById('content');
 	var link = document.getElementById('link');
+  var quoteButton = document.getElementById('quoteButton');
+  quoteButton.classList += " is-loading";
 
 	$.ajax({
       url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var quote = data[0];
-        console.log(quote);
         title.innerHTML = quote.title;
         content.innerHTML = quote.content;
         link.innerHTML = '<a href="' + quote.link + '">Source</a>';
+
+        var buttonClasses = quoteButton.classList.toString();
+        quoteButton.classList = buttonClasses.replace('is-loading', '');
       },
       cache: false
 	});
